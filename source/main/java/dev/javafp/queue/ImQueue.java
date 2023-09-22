c35dfbdc48d61c4c56ef39296679010f83b219f6
Van <github@vanemmenis.com>
2023-09-22 17:04:21 +0100

    Improved javadoc comments in ImQueue

--- source/main/java/dev/javafp/queue/ImQueue.java
+++ source/main/java/dev/javafp/queue/ImQueue.java
@@ -13,7 +13,7 @@ import dev.javafp.util.ImMaybe;
 import dev.javafp.val.ImValuesImpl;
 
 /**
- * <p> A queue
+ * <p> A queue that has a maximumm size.
  */
 public class ImQueue<A> extends ImValuesImpl
 {
@@ -23,6 +23,9 @@ public class ImQueue<A> extends ImValuesImpl
     // The list that is used to add elements to the queue
     private final ImList<A> two;
 
+    /**
+     * The maximum size for this queue
+     */
     public final int maxSize;
 
     private static ImQueue<?> empty = new ImQueue<>(0, ImList.on(), ImList.on());
@@ -34,6 +37,11 @@ public class ImQueue<A> extends ImValuesImpl
         this.maxSize = maxSize;
     }
 
+    /**
+     * <p> An empty queue with a maximum size of
+     * {@code maxSize}
+     *
+     */
     public static <A> ImQueue<A> ofSize(int maxSize)
     {
         return new ImQueue<>(maxSize, ImList.on(), ImList.on());
@@ -45,9 +53,11 @@ public class ImQueue<A> extends ImValuesImpl
     }
 
     /**
-     * Add `thing` to the end of the queue
+     * <p> Add
+     * {@code thing}
+     *  to the end of the queue
+     *
      */
-
     public ImQueue<A> addToEnd(A thing)
     {
         // If we are full, remove the head and add `thing`
@@ -102,33 +112,88 @@ public class ImQueue<A> extends ImValuesImpl
                               : ImPair.on(one.head(), onTwoLists(maxSize, one.tail(), two)));
     }
 
+    /**
+     * <p> A queue containing the elements of
+     * {@code as}
+     * and a
+     * {@code maxSize}
+     *  of
+     * {@code Integer.MAX_VALUE}
+     *
+     */
     public static <A> ImQueue<A> on(ImList<A> as)
     {
         return onTwoLists(Integer.MAX_VALUE, as, ImList.on());
     }
 
+    /**
+     * <p> A shortcut for
+     * {@code on(ImList.on(as)}
+     *
+     */
     @SafeVarargs
     public static <A> ImQueue<A> on(A... as)
     {
         return on(ImList.on(as));
     }
 
+    /**
+     * <p> A queue containing the elements of
+     * {@code elements}
+     * with a maximum size of
+     * {@code maxSize}
+     *
+     */
+    public static <A> ImQueue<A> onSize(int maxSize, ImList<A> elements)
+    {
+        return onTwoLists(maxSize, elements.take(maxSize), ImList.on());
+    }
+
+    /**
+     * <p> A shortcut for
+     * {@code onSize(maxSize, ImList.on(as)}
+     *
+     */
     @SafeVarargs
     public static <A> ImQueue<A> onSize(int maxSize, A... as)
     {
-        return onTwoLists(maxSize, ImList.on(as).take(maxSize), ImList.on());
+        return onSize(maxSize, ImList.on(as));
     }
 
+    /**
+     * <p> {@code true}
+     *  if
+     * {@code this}
+     *  is empty
+     *
+     */
     public boolean isEmpty()
     {
         return size() == 0;
     }
 
+    /**
+     * <p> A list containing the elements of
+     * {@code this}
+     *
+     */
     public ImList<A> toImList()
     {
         return one.append(two.reverse());
     }
 
+    /**
+     * <p> An
+     * {@link ImMaybe}
+     * containing the queue that is
+     * {@code this}
+     * with the first element removed or
+     * {@code Nothing}
+     *  if
+     * {@code this}
+     *  is empty.
+     *
+     */
     public ImMaybe<ImQueue<A>> removeFirst()
     {
         return split().flatMap(i -> ImMaybe.just(i.snd));
