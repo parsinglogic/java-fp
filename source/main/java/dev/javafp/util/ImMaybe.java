e386e68be572dd4c29419b6167a4e866e78fe19d
Van <github@vanemmenis.com>
2023-10-19 16:30:31 +0100

    Fixed javadoc comments in ImMaybe and added more tests

--- source/main/java/dev/javafp/util/ImMaybe.java
+++ source/main/java/dev/javafp/util/ImMaybe.java
@@ -33,6 +33,16 @@ import java.util.Objects;
  * {@code MaybeHasNothing}
  * exception.
  *
+ * <p> {@code ImMaybe}
+ *  ia a Monad so it implements
+ * {@code map}
+ * ,
+ * {@code flatmap}
+ *  and
+ * {@code join}
+ * .
+ *
+ *
  */
 public class ImMaybe<T> extends ImValuesImpl
 {
@@ -55,6 +65,17 @@ public class ImMaybe<T> extends ImValuesImpl
         this.value = value;
     }
 
+    /**
+     * <p> A
+     * {@code Maybe}
+     *  with the value
+     * {@code value}
+     * .
+     * <p> Throws {@link MaybeCannotHaveANullValueException} if
+     * {@code value == null}
+     * .
+     *
+     */
     public static <U> ImMaybe<U> just(U value)
     {
         if (value == null)
@@ -63,11 +84,35 @@ public class ImMaybe<T> extends ImValuesImpl
             return new ImMaybe(value);
     }
 
+    /**
+     * <p> An
+     * {@code ImMaybe}
+     *  with the value
+     * {@code valueOrNull}
+     * or
+     * {@code ImMaybe.nothing}
+     *  if
+     * {@code value == null}
+     * .
+     *
+     */
     public static <T> ImMaybe<T> with(T valueOrNull)
     {
         return with(null, valueOrNull);
     }
 
+    /**
+     * <p> An
+     * {@code ImMaybe}
+     *  with the value
+     * {@code valueOrBad}
+     * or
+     * {@code ImMaybe.nothing}
+     *  if
+     * {@code Equals.isEqual(bad, valueOrBad)}
+     * .
+     *
+     */
     public static <T> ImMaybe<T> with(T bad, T valueOrBad)
     {
         return Equals.isEqual(bad, valueOrBad)
@@ -75,6 +120,13 @@ public class ImMaybe<T> extends ImValuesImpl
                : just(valueOrBad);
     }
 
+    /**
+     * <p> The value in this
+     * {@code ImMaybe}
+     * .
+     * <p> If there is no value, throw {@link MaybeHasNothing}
+     *
+     */
     public T get()
     {
         if (value == null)
@@ -83,36 +135,33 @@ public class ImMaybe<T> extends ImValuesImpl
             return value;
     }
 
+    /**
+     * <p> Given an
+     * {@code ImMaybe}
+     *  whose value is itself an
+     * {@code ImMaybe}
+     * <p> return
+     *
+     * <pre>{@code
+     * m.flatMap(Fn.id())
+     * }</pre>
+     *
+     */
     public static <A> ImMaybe<A> join(ImMaybe<ImMaybe<A>> m)
     {
         return m.orElse(nothing);
     }
 
-    public boolean isPresent()
-    {
-        return value != null;
-    }
-
-    public <A> A ifPresentElse(Fn<T, A> yes, A not)
-    {
-        return isPresent()
-               ? yes.of(value)
-               : not;
-    }
-
-    public void ifPresentDo(FnConsumer<T> block)
-    {
-        if (isPresent())
-            block.doit(value);
-    }
-
-    public <U> ImMaybe<U> map(Fn<T, U> fn)
-    {
-        return !isPresent()
-               ? nothing
-               : ImMaybe.with(fn.of(value));
-    }
-
+    /**
+     * <p> Map the function
+     * {@code fn}
+     *  to  this
+     * {@code ImMaybe}
+     *  and then run
+     * {@link #join(ImMaybe)} on the result
+     *
+     *
+     */
     public <U> ImMaybe<U> flatMap(Fn<T, ImMaybe<U>> fn)
     {
         Objects.requireNonNull(fn);
@@ -120,6 +169,83 @@ public class ImMaybe<T> extends ImValuesImpl
         return join(this.map(fn));
     }
 
+    /**
+     * <p> {@code True}
+     *  if this
+     * {@code ImMaybe}
+     *  is not
+     * {@code ImMaybe.nothing}
+     * ,
+     * {@code false}
+     *  otherwise
+     *
+     */
+    public boolean isPresent()
+    {
+        return value != null;
+    }
+
+    /**
+     * <p> If there is a value in this
+     * {@code ImMaybe}
+     *  then apply the function
+     * {@code fn}
+     *  to the value and return the result.
+     * <p> If there is no value then return
+     * {@code not}
+     *
+     */
+    public <A> A ifPresentElse(Fn<T, A> fn, A not)
+    {
+        return isPresent()
+               ? fn.of(value)
+               : not;
+    }
+
+    /**
+     * <p> If there is a value in this
+     * {@code ImMaybe}
+     *  then apply the consumer function
+     * {@code block}
+     *  to the value
+     * <p> If there is no value then do nothing
+     *
+     */
+    public void ifPresentDo(FnConsumer<T> block)
+    {
+        if (isPresent())
+            block.doit(value);
+    }
+
+    /**
+     * <p> Apply the function
+     * {@code fn}
+     *  to the value and return an
+     * {@code ImMaybe}
+     *  with that value.
+     * <p> If that value is null then return
+     * {@code ImMaybe.nothing}
+     * .
+     * <p> If there is no value in this
+     * {@code ImMaybe}
+     *  then return
+     * {@code ImMaybe.nothing}
+     *
+     */
+    public <U> ImMaybe<U> map(Fn<T, U> fn)
+    {
+        return !isPresent()
+               ? nothing
+               : ImMaybe.with(fn.of(value));
+    }
+
+    /**
+     *
+     * <p> If there is a value in this
+     * {@code ImMaybe}
+     *  then return it, otherwise return
+     * {@code other}
+     */
     public T orElse(T other)
     {
         return value != null
@@ -127,6 +253,17 @@ public class ImMaybe<T> extends ImValuesImpl
                : other;
     }
 
+    /**
+     * <p> The representation of
+     * {@code this}
+     * as an {@link AbstractTextBox}
+     * <p> If the class extends {@link dev.javafp.val.ImValuesImpl} then the default
+     * {@code toString}
+     *  method will use this method
+     * and then convert the result to a
+     * {@code String}
+     *
+     */
     public AbstractTextBox getTextBox()
     {
         return ifPresentElse(v -> justBox.before(TextUtils.getBoxFrom(v)), nothingBox);
--- source/test/java/dev/javafp/util/ImMaybeTest.java
+++ source/test/java/dev/javafp/util/ImMaybeTest.java
@@ -1,12 +1,19 @@
 package dev.javafp.util;
 
+import dev.javafp.ex.MaybeHasNothing;
+import dev.javafp.lst.ImList;
 import junit.framework.TestCase;
 import org.junit.Test;
 
 import static dev.javafp.util.ImMaybe.just;
+import static dev.javafp.util.ImMaybe.nothing;
+import static dev.javafp.util.ImMaybe.with;
 
 public class ImMaybeTest extends TestCase
 {
+
+    String thing;
+
     @Test
     public void testJoin()
     {
@@ -17,6 +24,98 @@ public class ImMaybeTest extends TestCase
     @Test
     public void testFlatMap()
     {
-        assertEquals(just(1), ImMaybe.just(1).flatMap(i -> ImMaybe.with(i)));
+        assertEquals(just(1), ImMaybe.just(1).flatMap(i -> with(i)));
     }
+
+    @Test
+    public void testGetAndJust()
+    {
+        assertEquals("a", ImMaybe.just("a").get());
+
+        TestUtils.assertThrows(() -> nothing.get(), MaybeHasNothing.class);
+    }
+
+    @Test
+    public void testNothing()
+    {
+        assertFalse(nothing.isPresent());
+    }
+
+    @Test
+    public void tesEquals()
+    {
+        assertEquals(just("desserts"), just("desserts"));
+        assertEquals(nothing, nothing);
+    }
+
+    @Test
+    public void withNull()
+    {
+        assertEquals(nothing, with(null));
+    }
+
+    @Test
+    public void withOkAndBad()
+    {
+        assertEquals(just("bar"), with("foo", "bar"));
+        assertEquals(nothing, with("foo", "foo"));
+    }
+
+    @Test
+    public void testIsPresent()
+    {
+        assertEquals(false, nothing.isPresent());
+        assertEquals(true, just("william").isPresent());
+    }
+
+    @Test
+    public void testIfPresentElse()
+    {
+        assertEquals(0, nothing.ifPresentElse(i -> i, 0));
+        assertEquals(3, (int) just(2).ifPresentElse(i -> i + 1, 0));
+    }
+
+    @Test
+    public void testIfPresentDo()
+    {
+        just("a").ifPresentDo(i -> thing = i);
+
+        assertEquals("a", thing);
+
+        nothing.ifPresentDo(i -> thing = "b");
+
+        assertEquals("a", thing);
+
+    }
+
+    @Test
+    public void testMap()
+    {
+        assertEquals(just(18), just(17).map(i -> i + 1));
+        assertEquals(nothing, nothing.map(i -> i));
+    }
+
+    @Test
+    public void testOrElse()
+    {
+        assertEquals(18, (int) just(18).orElse(0));
+        assertEquals(0, nothing.orElse(0));
+    }
+
+    @Test
+    public void testTextBox()
+    {
+        assertEquals("Just 18", "" + just(18));
+        assertEquals("Nothing", "" + nothing);
+    }
+
+    @Test
+    public void testGetNamesAndValues()
+    {
+        assertEquals(ImList.on("value"), just(18).getNames());
+        assertEquals(ImList.on(18), just(18).getValues());
+        assertEquals(ImList.on((Integer) null), nothing.getValues());
+
+    }
+
 }
